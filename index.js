70365ef0da9042ca9264d90c676d9e0d34c65ea4
jayosh65 <jtoshin@gmail.com>
2020-08-08 16:06:29 +0100

    clean backend

--- index.js
+++ index.js
@@ -2,149 +2,15 @@ const express = require('express');
 const http = require("http");
 const socketIo = require("socket.io");
 const sqlite3 = require('sqlite3').verbose();
+const {
+  database_create,
+  database_insert,
+  database_update,
+  database_delete,
+  asyncCallQuery
+} = require('./sqlite-command');
 
-// let db = new sqlite3.Database('./db/todolist.db');
-// db.run('CREATE TABLE IF NOT EXISTS todos(NAME,TODOID,TASK)');
-// db.close();
-
-// open the database
-let db = new sqlite3.Database('./db/todolist.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the todo database.');
-});
-
-db.serialize(() => {
-  db.each(`SELECT Name as name, TODOID as todoid, TASK as task
-           FROM todos`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    // console.log(row.name);
-    console.log(row.todoid + "\t" + row.name  + "\t" + row.task);
-  });
-});
-
-var data = [
-  ['Mungade', '26', true],
-  ['Nagnath', '27', true]
-]
-//     db.run(`INSERT INTO todos(name, todoId, task) VALUES(?, ?, ?)`, [data], function(err) {
-//       if (err) {
-//         return console.log(err.message);
-//       }
-//       // get the last insert id
-//       console.log(`A row has been inserted with rowid ${this.lastID}`);
-//     });
-for (var i = 0; i < data.length; i++) {
-  db.run("INSERT INTO todos(name,todoid,task) values(?,?,?)", data[i][0], data[i][1], data[i][2], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    console.log('Insert Success');
-  })
-}
-
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
-
-// open the database
-// let db = new sqlite3.Database('./db/todolist.db', sqlite3.OPEN_READWRITE, (err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Connected to the todo database.');
-// });
-
-// db.serialize(() => {
-//   db.each(`SELECT TODOID as id,
-//                   Name as name
-//            FROM todos`, (err, row) => {
-//     if (err) {
-//       console.error(err.message);
-//     }
-//     console.log(row.id + "\t" + row.name);
-//   });
-// });
-
-// db.close((err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// });
-
-
-
-// db.close();
-// // open the database
-// let db = new sqlite3.Database('./db/todolist.db', sqlite3.OPEN_READWRITE, (err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Connected to the todo database.');
-// });
-
-// db.serialize(() => {
-//   db.each(`SELECT TODOtodoId as todoId,
-//                   Name as name
-//            FROM todo`, (err, row) => {
-//     if (err) {
-//       console.error(err.message);
-//     }
-//     console.log(row.id + "\t" + row.name);
-//   });
-// });
-
-// db.close((err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Close the todo connection.');
-// });
-
-// // Create embedded table
-// const db = new sqlite3.Database('./todolist.db');
-// const createTable = "CREATE TABLE IF NOT EXISTS todos ('name' text NOT NULL, 'todoId' varchar PRIMARY KEY, complete_task BOOLEAN );"
-// db.run(createTable, function(err) {
-//   if (err) {
-//     console.log(err.message);
-//   }
-//   console.log('Table created')
-// });
-
-// const insert_table = data => {
-//   // db.run(`INSERT into todos(todoId, name, complete_task) values(${data.id}, ${data.text}, ${data.complete})`, function(err) {
-//   db.run("INSERT INTO todos(todoId, name, complete_task) values(test, testtest, 0)", function(err) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//     console.log('Table inserted')
-//   });
-// }
-
-// const query_table = () => {
-//   db.each("SELECT rowId AS name FROM todos", function(err, row) {
-//   // db.all("SELECT todoId FROM todos", function(err, rows) {
-//     // if (err) {
-//     //   console.log(err.message);
-//     // }
-//     console.log(row.name);
-//     // console.log(row.todoId + ": " + row.name + ": " + row.complete_task);
-//     // console.log(err)
-//   });
-// }
-
-// db.close();
-
-
-
-
+database_create();
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
@@ -154,46 +20,51 @@ app.use(index);
 
 const server = http.createServer(app);
 
-// const io = socketIo(server);
+const io = socketIo(server);
 
-// let interval;
+let interval;
 
+io.on('connection', socket => {
+  // Query database
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
 
+  console.log('a user connected');
 
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   if (interval) {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(() => getApiAndEmit(socket), 1000);
-//   socket.on("add list", data => {
-//     // insert one row into the todo table
-//     db.run(`INSERT INTO todos(name, todoId, task) VALUES(?, ?, ?)`, [data], function(err) {
-//       if (err) {
-//         return console.log(err.message);
-//       }
-//       // get the last insert id
-//       console.log(`A row has been inserted with rowid ${this.lastID}`);
-//     });
+  socket.on('add todo', (data) => {
+    database_insert(data.id, data.name, data.task);
+    console.log('Todo with an id of ' + data.id  + 'was added.');
+  });
 
-//     // close the database connection
+  socket.on('remove todo', (id) => {
+    database_delete(id);
+    console.log('Todo with an id of ' + id + ' was removed.');
+  });
 
-//     console.log('Add to list');
-//   });
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//     clearInterval(interval);
-//   });
-// });
-// // db.close();
-// const getApiAndEmit = socket => {
-//   const response = new Date();
+  socket.on('update todo', (data) => {
+    database_update(data.id, data.name, data.task);
+    console.log('Todo with an id of ' + data.id + ' was updated.');
+  });
 
-//   // Emitting a new message. Will be consumed by the client
-//   socket.emit("FromAPI", response);
-//   // query_table();
-// };
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    clearInterval(interval);
+  });
+});
 
-// db.close();
+const getApiAndEmit = socket => {
+  let db = new sqlite3.Database('./db/todolist.db');
+  db.all('SELECT name as name, todoId as id, task as task FROM todos', [], (err, rows) => {
+      if (err) {
+          return console.error(err.message);
+      }
+      console.log('Data received from Db:\n');
+      console.log(rows);
+      socket.emit('showrows', rows);
+  })
+  db.close();
+};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ sqlite-command.js
@@ -0,0 +1,103 @@
+const sqlite3 = require('sqlite3').verbose();
+let data = [];
+
+// Create database and table
+function database_create() {
+    let db = new sqlite3.Database('./db/todolist.db');
+    db.run('CREATE TABLE IF NOT EXISTS todos(NAME,TODOID,TASK)');
+    console.log('Database and table created.');
+    db.close();
+}
+
+// Query database
+function database_query() {
+    return new Promise((resolve, reject) => {
+        let db = new sqlite3.Database('./db/todolist.db');
+        db.all('SELECT name as name, todoId as id, task as task FROM todos', [], (err, rows) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            rows.forEach(row => {
+                console.log(row);
+                data.push(row);
+            });
+            resolve(data);
+        })
+    })
+}
+
+// Query call back
+async function asyncCallQuery() {
+    console.log('calling');
+    const result = await database_query();
+    return result;
+}
+
+// Insert into database
+function database_insert(id, name, complete) {
+    let db = new sqlite3.Database('./db/todolist.db');
+
+    // insert one row into the langs table
+    db.run(`INSERT INTO todos(NAME,TODOID,TASK) VALUES(?, ?, ?)`, [name, id, complete], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+
+    db.close();
+}
+
+// Update row in database
+function database_update(id, name, complete) {
+    let db = new sqlite3.Database('./db/todolist.db');
+
+    // insert one row into the langs table
+    db.run(`UPDATE todos
+            SET NAME = $name, TODOID = $todoid, TASK = $task 
+            WHERE TODOID = $todoid`, { $name: name, $todoid: id, $task: complete }, function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.changes}`);
+    });
+
+    db.close();
+}
+
+// Remove row
+function database_delete(id) {
+    const sqlite3 = require('sqlite3').verbose();
+
+    // open a database connection
+    let db = new sqlite3.Database('./db/todolist.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+
+    // delete a row based on id
+    db.run(`DELETE FROM todos WHERE TODOID=?`, id, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Row(s) deleted ${this.changes}`);
+    });
+
+    // close the database connection
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
+}
+
+module.exports = {
+    database_create,
+    asyncCallQuery,
+    database_insert,
+    database_update,
+    database_delete
+}
